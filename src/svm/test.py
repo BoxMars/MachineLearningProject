27c7260350e1099ffdb9affd30ff4b2033e4f23b
Zhu-s-m <70746869+Zhu-s-m@users.noreply.github.com>
2021-11-21 23:35:32 +0800

    Update test.py

--- src/svm/test.py
+++ src/svm/test.py
@@ -6,14 +6,16 @@ import time
 
 def main():
     data = Data()
-    x_train, y_train = data.get_data(data.TRAIN_DIR)
-    x_test, y_test = data.get_data(data.TEST_DIR)
+    x_train, y_train = data.get_data(data.CROP_DIR)
+    # x_test, y_test = data.get_data(data.TEST_DIR)
     pca = decomposition.IncrementalPCA()
     # pca.fit(x_train,y_train)
     # trainW = pca.transform(x_train)  # fit the training set
     trainW = pca.fit_transform(x_train)  # fit the training set
+    pca2=decomposition.IncrementalPCA()
+    trainW=pca2.fit_transform(trainW)
     svmclf = svm.SVC(kernel='rbf')
-    scores = cross_val_score(svmclf, trainW, y_train, cv=3).mean()
+    scores = cross_val_score(svmclf, trainW, y_train, cv=3)
     print("cross valid kernel pca + svm =", scores)
 
 
